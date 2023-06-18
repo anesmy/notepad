f185a667f6f29113ca6e80a0432c868358540330
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-06-18 12:20:42 +0300

    Initial commit

---
+++ src/test/java/com/nesmy/notepad/NotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesmy.notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
